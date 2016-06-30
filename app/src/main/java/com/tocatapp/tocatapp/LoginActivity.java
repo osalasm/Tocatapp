70e57906426ab0e2093f790e68d0caf46baef503
Omar Salas <o.salas.mar@gmail.com>
2016-06-30 12:09:10 -0400

    Profile layout

--- app/src/main/java/com/tocatapp/tocatapp/LoginActivity.java
+++ app/src/main/java/com/tocatapp/tocatapp/LoginActivity.java
@@ -6,6 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Omar on 28-06-2016.
@@ -32,16 +36,35 @@ public class LoginActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void OnClick(View v) {
-                String userMail = "Augusto";
-                String userPassword = "1234";
                 final String editTextMail.getText().toString();
                 final String editTextPassword.getText().toString();
 
-                if(editTextMail.equals(userMail) & userPassword.equals(userPassword)) {
-
+                if(!validateMail(editTextMail.getText().toString())) {
+                    editTextMail.setError("Mail invalido");
+                    editTextMail.requestFocus();
+                } else if(!validatePassword(editTextPassword.getText().ToString())) {
+                    editTextPassword.setError("Contraseña invalida");
+                    editTextPassword.requestFocus();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Validacion correcta", Toast.LENGTH_LONG).show();
                 }
 
             }
         });
+    }
+    protected boolean validatePassword(String editTextPassword) {
+        if(editTextPassword != null && editTextPassword.length() > 6) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    protected boolean validateMail(String editTextMail) {
+        String mailPattern = "^[_A-Z-z0-9-\\+]+(\\.[_A-Z-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+        Pattern pattern = Pattern.compile(mailPattern);
+        Matcher matcher = pattern.matcher(editTextMail);
+        return matcher.matches();
     }
 }
